360571dd20ec18346219fe9c27aea9e551cd81e2
Josh Bassett <josh.bassett@gmail.com>
2019-03-21 11:15:54 +1100

    Refactor placementEngine

--- src/placementEngine.js
+++ src/placementEngine.js
@@ -1,3 +1,4 @@
+const always = require('fkit/dist/always')
 const compose = require('fkit/dist/compose')
 const filter = require('fkit/dist/filter')
 const groupBy = require('fkit/dist/groupBy')
@@ -12,7 +13,7 @@ const match = require('./match')
  * promo constraints and the client state object.
  *
  * All promos with constraints will be matched with the client state object.
- * Any promos with matching constraints will be placed, otherwise they will be
+ * Promos with matching constraints will be placed, otherwise they will be
  * ignored.
  *
  * @params {Object} state The client state object.
@@ -20,16 +21,27 @@ const match = require('./match')
  * @returns {Array} The list of placed promos.
  */
 function placementEngine (state, promos) {
-  // Filters promos that have matching constraints.
-  const f = filter(promo => promo.constraints ? match(promo.constraints)(state) : true)
+  // Filter the promos that have matching constraints.
+  const f = filter(promo => {
+    // Promos without constraints are passed through.
+    const predicate = promo.constraints ? match(promo.constraints) : always(true)
+    return predicate(state)
+  })
 
-  // Groups the promos by groupId.
+  // Group the promos by groupId.
   const g = groupBy((a, b) => a.groupId === b.groupId)
 
-  // Chooses one random promo from each group.
+  // Choose one random promo from each group.
+  //
+  // Only one promo from each group may be placed at the same time. This allows
+  // us to randomly cycle through multiple variants of a promo.
   const h = map(group => head(sample(1, group)))
 
-  return compose(h, g, f)(promos)
+  // Compose the transform functions and apply the promos to get the list of
+  // placed promos.
+  const placedPromos = compose(h, g, f)(promos)
+
+  return placedPromos
 }
 
 module.exports = placementEngine
