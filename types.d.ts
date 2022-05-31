ac2fbe28053483aa28031d01698f90a6bca67aa8
Tiago Amaro <tiagopadrela@gmail.com>
2022-05-31 15:32:59 -0300

    Create the types.d.ts
    We are not declaring any types for the `promos-client` at the moment,
    but declaring an empty module will avoid TypeScript compilation
    error code 7016, which assumes we are implicitly declaring that
    exports have the "any" type.
    
    We are now explicitly declaring it has the "any" type.

---
+++ types.d.ts
@@ -0,0 +1,4 @@
+// `@theconversation/promos-client` is a JS project without typescript definitions.
+// Let's declare its exports with the "any" type, so TS can explicitly infer types for it, avoiding
+// TS compiler error code 7016.
+declare module "@theconversation/promos-client"
